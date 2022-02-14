46bd94699654b75fdc0a9b3431de3a95e2d4f096
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2022-02-14 14:34:54 +0600

    fix xcframework build
    after merge, bazel's output now in bazel-out with uniq ids

--- BUILD_FACE_MESH_XCFRAMEWORK.sh
+++ BUILD_FACE_MESH_XCFRAMEWORK.sh
@@ -17,10 +17,13 @@ bazel build --copt=-fembed-bitcode --apple_bitcode=embedded --config=ios_arm64 m
 # The arm64 framework zip will be located at //bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.zip
 
 # Call the framework patcher (First argument = compressed framework.zip, Second argument = header file's name(in this case FaceMeshIOSLib.h))
-./mediapipe/examples/ios/facemeshioslib/patch_ios_framework.sh ./bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.zip FaceMeshIOSLib.h
+
+arm_zip=$(find bazel-out/ -iname "FaceMeshIOSLibFramework.zip" | grep arm)
+./mediapipe/examples/ios/facemeshioslib/patch_ios_framework.sh $arm_zip FaceMeshIOSLib.h
 
 # There will be a resulting patched .framework folder at the same directory, this is our arm64 one, we copy it to our arm64 folder
-cp -a ./bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.framework ./frameworkbuild/FaceMeshIOSLibFramework/arm64
+arm_framework=$(find bazel-out/ -iname "FaceMeshIOSLibFramework.framework" | grep arm)
+cp -a $arm_framework ./frameworkbuild/FaceMeshIOSLibFramework/arm64
 
 # Do the same for x86_64
 
@@ -28,10 +31,12 @@ cp -a ./bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.
 bazel build --copt=-fembed-bitcode --apple_bitcode=embedded --config=ios_x86_64 mediapipe/examples/ios/facemeshioslib:FaceMeshIOSLibFramework
 
 # Call the framework patcher
-./mediapipe/examples/ios/facemeshioslib/patch_ios_framework.sh ./bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.zip FaceMeshIOSLib.h
+x86_zip=$(find bazel-out/ -iname "FaceMeshIOSLibFramework.zip" | grep x86_64)
+./mediapipe/examples/ios/facemeshioslib/patch_ios_framework.sh $x86_zip FaceMeshIOSLib.h
 
 # copy the patched framework to our folder
-cp -a ./bazel-bin/mediapipe/examples/ios/facemeshioslib/FaceMeshIOSLibFramework.framework ./frameworkbuild/FaceMeshIOSLibFramework/x86_64
+x86_framework=$(find bazel-out/ -iname "FaceMeshIOSLibFramework.framework" | grep x86_64)
+cp -a $x86_framework ./frameworkbuild/FaceMeshIOSLibFramework/x86_64
 
 # Create xcframework (because the classic lipo method with normal .framework no longer works (shows Building for iOS Simulator, but the linked and embedded framework was built for iOS + iOS Simulator))
 
